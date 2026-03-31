3a44a53246340649b30dc87217f0396e6c59fc7d
QQ <lucaquade@gmail.com>
2026-03-31 19:17:37 +0200

    Changed available algorithms and datasets

--- hpc_algorithm_names.py
+++ hpc_algorithm_names.py
@@ -1,3 +1,4 @@
-algorithm_names = ['BPR', 'CDAE', 'DMF', 'EASE', 'ENMF', 'ImplicitMF', 'ItemItem', 'ItemKNN', 'ItemKNNRP', 'LightGCN',
-                   'LINE', 'MacridVAE', 'MultiDAE', 'MultiVAE', 'NCEPLRec', 'NeuMF', 'NMF', 'Pop', 'PopScore', 'Random',
+algorithm_names = ['BPR', 'CDAE', 'DMF', 'EASE', 'ENMF', 'ImplicitMF', 'ItemItem', 'ItemKNN', 'ItemKNNRP', 
+		   'LINE', 'MacridVAE', 'MultiDAE', 'MultiVAE', 'NCEPLRec', 'NeuMF', 'NMF', 'Pop', 'PopScore', 'Random',
                    'RecVAE', 'SimpleX', 'SVD', 'UserUser']
+
